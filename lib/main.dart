5661fabd652648da71e627b804b11eb21ac6bc4d
akhil <akhilsodvadiya7@gmail.com>
2024-10-22 13:16:31 +0530

    22-10-2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,55 @@
-import 'package:adv_flutter_ch1/views/stepper_home_page.dart';
+import 'package:adv_flutter_ch1/provider/change_theme_provider.dart';
+import 'package:adv_flutter_ch1/views/change_theme_home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+bool theme = false;
+bool isHomed = false;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+
+  theme = sharedPreferences.getBool('theme') ?? false;
+  isHomed = sharedPreferences.getBool('home') ?? false;
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => ChangeThemeScreenProvider(theme),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => ChangeThemeScreenProvider(theme),
+        ),
+      ],
+      builder: (context, child) => const MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
+    ChangeThemeScreenProvider changeThemeScreenProviderTrue =
+    Provider.of<ChangeThemeScreenProvider>(context, listen: true);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const HomePage(),
+      home: const ChangeTheme(),
+      theme: changeThemeDataToLight,
+      darkTheme: changeThemeDataToDark,
+      themeMode: changeThemeScreenProviderTrue.isDark
+          ? ThemeMode.dark
+          : ThemeMode.light,
     );
+
   }
 }
